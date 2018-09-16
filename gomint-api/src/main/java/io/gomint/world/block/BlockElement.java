17b004447ddb764ba313156e8392deef774c322f
Kaooot <jonapapenfuss@gmail.com>
2018-09-16 16:27:30 +0200

    Added BlockElement to the api

---
+++ gomint-api/src/main/java/io/gomint/world/block/BlockElement.java
@@ -0,0 +1,24 @@
+package io.gomint.world.block;
+
+/**
+ * @author Kaooot
+ * @version 1.0
+ */
+public interface BlockElement extends Block {
+
+    int getProtons();
+
+    int getElectrons();
+
+    int getNeutrons();
+
+    int getAtomicWeight();
+
+    String getName();
+
+    String getSymbol();
+
+    String getMetalKind();
+
+    float getAtomicMass();
+}
--- gomint-server/src/main/java/io/gomint/server/inventory/item/ItemLithium.java
+++ gomint-server/src/main/java/io/gomint/server/inventory/item/ItemLithium.java
@@ -3,6 +3,10 @@ package io.gomint.server.inventory.item;
 import io.gomint.inventory.item.ItemType;
 import io.gomint.server.registry.RegisterInfo;
 
+/**
+ * @author Kaooot
+ * @version 1.0
+ */
 @RegisterInfo( id = 269 )
 public class ItemLithium extends ItemStack {
 
