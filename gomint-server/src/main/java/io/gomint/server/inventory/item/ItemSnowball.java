e8aa41bbc3d1fc5e5fafa9946ffaebaf5d8e6c2a
Clockw1seLrd <clockw1selrd@gmail.com>
2018-10-03 18:45:04 +0200

    Do not subtract item amount if player is in creative

--- gomint-server/src/main/java/io/gomint/server/inventory/item/ItemSnowball.java
+++ gomint-server/src/main/java/io/gomint/server/inventory/item/ItemSnowball.java
@@ -9,6 +9,7 @@ import io.gomint.server.entity.EntityPlayer;
 import io.gomint.server.entity.projectile.EntitySnowball;
 import io.gomint.server.registry.RegisterInfo;
 import io.gomint.server.world.WorldAdapter;
+import io.gomint.world.Gamemode;
 import io.gomint.world.block.Block;
 import io.gomint.world.block.BlockFace;
 
@@ -38,13 +39,15 @@ public class ItemSnowball extends ItemStack implements io.gomint.inventory.item.
                                 snowball.getPitch() );
         }
 
-        // Subtract amount
-        int newAmount = this.getAmount() - 1;
-        if ( newAmount == 0 ) {
-            entity.getInventory().setItem( entity.getInventory().getItemInHandSlot(), ItemAir.create( 0 ) );
-        } else {
-            this.setAmount( newAmount );
-            entity.getInventory().setItem( entity.getInventory().getItemInHandSlot(), this );
+        if ( entity.getGamemode() != Gamemode.CREATIVE ) {
+            // Subtract amount
+            int newAmount = this.getAmount() - 1;
+            if ( newAmount == 0 ) {
+                entity.getInventory().setItem( entity.getInventory().getItemInHandSlot(), ItemAir.create( 0 ) );
+            } else {
+                this.setAmount( newAmount );
+                entity.getInventory().setItem( entity.getInventory().getItemInHandSlot(), this );
+            }
         }
 
         return true;
