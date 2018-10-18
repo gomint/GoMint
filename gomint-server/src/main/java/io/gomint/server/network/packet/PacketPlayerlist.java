92d468b23e13a817e5016cce469c4e42720e8b57
Kevims <kcodeyt@gmail.com>
2018-10-18 05:01:43 +0200

    Set name in Playerlist to display name

--- gomint-server/src/main/java/io/gomint/server/network/packet/PacketPlayerlist.java
+++ gomint-server/src/main/java/io/gomint/server/network/packet/PacketPlayerlist.java
@@ -84,7 +84,7 @@ public class PacketPlayerlist extends Packet {
         private PlayerSkin skin;
 
         public Entry( EntityHuman human ) {
-            this( human.getUUID(), human.getEntityId(), human.getName(), human.getXboxID(), human.getSkin() );
+            this( human.getUUID(), human.getEntityId(), human.getDisplayName(), human.getXboxID(), human.getSkin() );
         }
     }
 
