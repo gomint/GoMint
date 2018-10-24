b3b58aeb65eb009482ef3cc7e28c6d5585c38245
Clockw1seLrd <clockw1selrd@gmail.com>
2018-10-24 22:57:41 +0200

    Implement burn times for sticks

--- gomint-api/src/main/java/io/gomint/inventory/item/ItemStick.java
+++ gomint-api/src/main/java/io/gomint/inventory/item/ItemStick.java
@@ -6,7 +6,7 @@ import io.gomint.GoMint;
  * @author geNAZt
  * @version 1.0
  */
-public interface ItemStick extends ItemStack {
+public interface ItemStick extends ItemStack, ItemBurnable {
 
     /**
      * Create a new item stack with given class and amount
@@ -15,6 +15,11 @@ public interface ItemStick extends ItemStack {
      */
     static ItemStick create( int amount ) {
         return GoMint.instance().createItemStack( ItemStick.class, amount );
+    }
+
+    @Override
+    default long getBurnTime() {
+        return 5000;
     }
 
 }
