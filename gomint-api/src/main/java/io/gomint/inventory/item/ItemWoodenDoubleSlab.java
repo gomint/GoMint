86724107ee352367a69b3bb811a5a9c55f2d5133
Clockw1seLrd <clockw1selrd@gmail.com>
2018-10-24 22:34:55 +0200

    Implement wooden double slabs burn times for the furnace

--- gomint-api/src/main/java/io/gomint/inventory/item/ItemWoodenDoubleSlab.java
+++ gomint-api/src/main/java/io/gomint/inventory/item/ItemWoodenDoubleSlab.java
@@ -6,7 +6,7 @@ import io.gomint.GoMint;
  * @author geNAZt
  * @version 1.0
  */
-public interface ItemWoodenDoubleSlab extends ItemStack {
+public interface ItemWoodenDoubleSlab extends ItemStack, ItemBurnable {
 
     /**
      * Create a new item stack with given class and amount
@@ -15,6 +15,11 @@ public interface ItemWoodenDoubleSlab extends ItemStack {
      */
     static ItemWoodenDoubleSlab create( int amount ) {
         return GoMint.instance().createItemStack( ItemWoodenDoubleSlab.class, amount );
+    }
+
+    @Override
+    default long getBurnTime() {
+        return 15000;
     }
 
 }
