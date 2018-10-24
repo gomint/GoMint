8de9152b9fc7ef7d3b340f21f3769857e39e31cd
Clockw1seLrd <clockw1selrd@gmail.com>
2018-10-24 22:45:48 +0200

    Implement burn times for jukeboxes

--- gomint-api/src/main/java/io/gomint/inventory/item/ItemJukebox.java
+++ gomint-api/src/main/java/io/gomint/inventory/item/ItemJukebox.java
@@ -13,7 +13,7 @@ import io.gomint.GoMint;
  * @author geNAZt
  * @version 1.0
  */
-public interface ItemJukebox extends ItemStack {
+public interface ItemJukebox extends ItemStack, ItemBurnable {
 
     /**
      * Create a new item stack with given class and amount
@@ -22,6 +22,11 @@ public interface ItemJukebox extends ItemStack {
      */
     static ItemJukebox create( int amount ) {
         return GoMint.instance().createItemStack( ItemJukebox.class, amount );
+    }
+
+    @Override
+    default long getBurnTime() {
+        return 15000;
     }
 
 }
