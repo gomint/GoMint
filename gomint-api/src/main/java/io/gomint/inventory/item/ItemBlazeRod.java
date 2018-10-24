be9c51c69747f7d723841995f33461c4a92fc2f7
Clockw1seLrd <clockw1selrd@gmail.com>
2018-10-24 22:28:30 +0200

    Implement blaze rod burn times for the furnace

--- gomint-api/src/main/java/io/gomint/inventory/item/ItemBlazeRod.java
+++ gomint-api/src/main/java/io/gomint/inventory/item/ItemBlazeRod.java
@@ -6,7 +6,7 @@ import io.gomint.GoMint;
  * @author geNAZt
  * @version 1.0
  */
-public interface ItemBlazeRod extends ItemStack {
+public interface ItemBlazeRod extends ItemStack, ItemBurnable {
 
     /**
      * Create a new item stack with given class and amount
@@ -15,6 +15,11 @@ public interface ItemBlazeRod extends ItemStack {
      */
     static ItemBlazeRod create( int amount ) {
         return GoMint.instance().createItemStack( ItemBlazeRod.class, amount );
+    }
+
+    @Override
+    default long getBurnTime() {
+        return 120000;
     }
 
 }
