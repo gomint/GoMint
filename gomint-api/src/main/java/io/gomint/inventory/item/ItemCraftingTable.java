e4a46d57877971dd30f8c53a26c9723ba9f85fa8
Clockw1seLrd <clockw1selrd@gmail.com>
2018-10-24 22:43:39 +0200

    Implement burn times for crafting tables

--- gomint-api/src/main/java/io/gomint/inventory/item/ItemCraftingTable.java
+++ gomint-api/src/main/java/io/gomint/inventory/item/ItemCraftingTable.java
@@ -6,7 +6,7 @@ import io.gomint.GoMint;
  * @author geNAZt
  * @version 1.0
  */
-public interface ItemCraftingTable extends ItemStack {
+public interface ItemCraftingTable extends ItemStack, ItemBurnable {
 
     /**
      * Create a new item stack with given class and amount
@@ -15,6 +15,11 @@ public interface ItemCraftingTable extends ItemStack {
      */
     static ItemCraftingTable create( int amount ) {
         return GoMint.instance().createItemStack( ItemCraftingTable.class, amount );
+    }
+
+    @Override
+    default long getBurnTime() {
+        return 15000;
     }
 
 }
