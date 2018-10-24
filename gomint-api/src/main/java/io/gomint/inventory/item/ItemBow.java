3bc48560c2592e480a24c6881b8f3fd3b0117daf
Clockw1seLrd <clockw1selrd@gmail.com>
2018-10-24 22:48:14 +0200

    Implement burn times for bows

--- gomint-api/src/main/java/io/gomint/inventory/item/ItemBow.java
+++ gomint-api/src/main/java/io/gomint/inventory/item/ItemBow.java
@@ -6,7 +6,7 @@ import io.gomint.GoMint;
  * @author geNAZt
  * @version 1.0
  */
-public interface ItemBow extends ItemStack {
+public interface ItemBow extends ItemStack, ItemBurnable {
 
     /**
      * Create a new item stack with given class and amount
@@ -15,6 +15,11 @@ public interface ItemBow extends ItemStack {
      */
     static ItemBow create( int amount ) {
         return GoMint.instance().createItemStack( ItemBow.class, amount );
+    }
+
+    @Override
+    default long getBurnTime() {
+        return 10000;
     }
 
 }
