3a9080a53960180dfac7f086d1429e500a61e57f
Clockw1seLrd <clockw1selrd@gmail.com>
2018-10-24 22:37:33 +0200

    Implement wooden pressure plate burn times for the furnace

--- gomint-api/src/main/java/io/gomint/inventory/item/ItemWoodenPressurePlate.java
+++ gomint-api/src/main/java/io/gomint/inventory/item/ItemWoodenPressurePlate.java
@@ -6,7 +6,7 @@ import io.gomint.GoMint;
  * @author geNAZt
  * @version 1.0
  */
-public interface ItemWoodenPressurePlate extends ItemStack {
+public interface ItemWoodenPressurePlate extends ItemStack, ItemBurnable {
 
     /**
      * Create a new item stack with given class and amount
@@ -15,6 +15,11 @@ public interface ItemWoodenPressurePlate extends ItemStack {
      */
     static ItemWoodenPressurePlate create( int amount ) {
         return GoMint.instance().createItemStack( ItemWoodenPressurePlate.class, amount );
+    }
+
+    @Override
+    default long getBurnTime() {
+        return 15000;
     }
 
 }
