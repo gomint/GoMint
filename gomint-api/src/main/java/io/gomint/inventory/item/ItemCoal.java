82d90c3ed47ceeadbc0da9e1a35635944cb58a02
Clockw1seLrd <clockw1selrd@gmail.com>
2018-10-24 22:29:23 +0200

    Move ItemBurnable implementation to the API interface

--- gomint-api/src/main/java/io/gomint/inventory/item/ItemCoal.java
+++ gomint-api/src/main/java/io/gomint/inventory/item/ItemCoal.java
@@ -6,7 +6,7 @@ import io.gomint.GoMint;
  * @author geNAZt
  * @version 1.0
  */
-public interface ItemCoal extends ItemStack {
+public interface ItemCoal extends ItemStack, ItemBurnable {
 
     /**
      * Create a new item stack with given class and amount
@@ -15,6 +15,11 @@ public interface ItemCoal extends ItemStack {
      */
     static ItemCoal create( int amount ) {
         return GoMint.instance().createItemStack( ItemCoal.class, amount );
+    }
+
+    @Override
+    default long getBurnTime() {
+        return 80000;
     }
 
 }
