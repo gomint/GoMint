311b91921abef25d1b174e1705d043dad994293b
Clockw1seLrd <clockw1selrd@gmail.com>
2018-11-02 20:43:27 +0100

    Replace star import with single imports

--- gomint-api/src/main/java/io/gomint/command/annotation/Overloads.java
+++ gomint-api/src/main/java/io/gomint/command/annotation/Overloads.java
@@ -1,6 +1,9 @@
 package io.gomint.command.annotation;
 
-import java.lang.annotation.*;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 /**
  * @author geNAZt
