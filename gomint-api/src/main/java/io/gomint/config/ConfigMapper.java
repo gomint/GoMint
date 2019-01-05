f9134ea498eaaa8ca83c28910b18725e83251aad
Fabian Faßbender <fabian.fassbender42@googlemail.com>
2019-01-05 14:52:26 +0100

    Make sonar more happy

--- gomint-api/src/main/java/io/gomint/config/ConfigMapper.java
+++ gomint-api/src/main/java/io/gomint/config/ConfigMapper.java
@@ -21,20 +21,20 @@ import java.util.Map;
  */
 public class ConfigMapper extends BaseConfigMapper {
 
-    @SuppressWarnings( "unchecked" )
-    public Map<String, Object> saveToMap( Class clazz ) throws Exception {
+    @SuppressWarnings("unchecked")
+    public Map<String, Object> saveToMap(Class clazz) throws Exception {
         Map<String, Object> returnMap = new HashMap<>();
 
-        if ( !clazz.getSuperclass().equals( YamlConfig.class ) && !clazz.getSuperclass().equals( Object.class ) ) {
-            Map<String, Object> map = this.saveToMap( clazz.getSuperclass() );
+        if (!clazz.getSuperclass().equals(YamlConfig.class) && !clazz.getSuperclass().equals(Object.class)) {
+            Map<String, Object> map = this.saveToMap(clazz.getSuperclass());
 
-            for ( Map.Entry<String, Object> entry : map.entrySet() ) {
-                returnMap.put( entry.getKey(), entry.getValue() );
+            for (Map.Entry<String, Object> entry : map.entrySet()) {
+                returnMap.put(entry.getKey(), entry.getValue());
             }
         }
 
-        for ( Field field : clazz.getDeclaredFields() ) {
-            if ( this.doSkip( field ) ) {
+        for (Field field : clazz.getDeclaredFields()) {
+            if (this.doSkip(field)) {
                 continue;
             }
 
@@ -42,15 +42,15 @@ public class ConfigMapper extends BaseConfigMapper {
 
             switch (configMode) {
                 case PATH_BY_UNDERSCORE:
-                    path = field.getName().replace( "_", "." );
+                    path = field.getName().replace("_", ".");
                     break;
                 case FIELD_IS_KEY:
                     path = field.getName();
                     break;
                 case DEFAULT:
                 default:
-                    if ( field.getName().contains( "_" ) ) {
-                        path = field.getName().replace( "_", "." );
+                    if (field.getName().contains("_")) {
+                        path = field.getName().replace("_", ".");
                     } else {
                         path = field.getName();
                     }
@@ -58,32 +58,32 @@ public class ConfigMapper extends BaseConfigMapper {
                     break;
             }
 
-            if ( field.isAnnotationPresent( Path.class ) ) {
-                path = field.getAnnotation( Path.class ).value();
+            if (field.isAnnotationPresent(Path.class)) {
+                path = field.getAnnotation(Path.class).value();
             }
 
-            if ( Modifier.isPrivate( field.getModifiers() ) ) {
-                field.setAccessible( true );
+            if (Modifier.isPrivate(field.getModifiers())) {
+                field.setAccessible(true);
             }
 
             try {
-                returnMap.put( path, field.get( this ) );
-            } catch ( IllegalAccessException ignored ) {
+                returnMap.put(path, field.get(this));
+            } catch (IllegalAccessException ignored) {
 
             }
         }
 
-        Converter mapConverter = converter.getConverter( Map.class );
-        return (Map<String, Object>) mapConverter.toConfig( HashMap.class, returnMap, null );
+        Converter mapConverter = converter.getConverter(Map.class);
+        return (Map<String, Object>) mapConverter.toConfig(HashMap.class, returnMap, null);
     }
 
-    public void loadFromMap( Map section, Class clazz ) throws Exception {
-        if ( !clazz.getSuperclass().equals( YamlConfig.class ) && !clazz.getSuperclass().equals( YamlConfig.class ) ) {
-            this.loadFromMap( section, clazz.getSuperclass() );
+    public void loadFromMap(Map section, Class clazz) throws Exception {
+        if (!clazz.getSuperclass().equals(YamlConfig.class)) {
+            this.loadFromMap(section, clazz.getSuperclass());
         }
 
-        for ( Field field : clazz.getDeclaredFields() ) {
-            if ( this.doSkip( field ) ) {
+        for (Field field : clazz.getDeclaredFields()) {
+            if (this.doSkip(field)) {
                 continue;
             }
 
@@ -91,15 +91,15 @@ public class ConfigMapper extends BaseConfigMapper {
 
             switch (configMode) {
                 case PATH_BY_UNDERSCORE:
-                    path = field.getName().replace( "_", "." );
+                    path = field.getName().replace("_", ".");
                     break;
                 case FIELD_IS_KEY:
                     path = field.getName();
                     break;
                 case DEFAULT:
                 default:
-                    if ( field.getName().contains( "_" ) ) {
-                        path = field.getName().replace( "_", "." );
+                    if (field.getName().contains("_")) {
+                        path = field.getName().replace("_", ".");
                     } else {
                         path = field.getName();
                     }
@@ -107,15 +107,15 @@ public class ConfigMapper extends BaseConfigMapper {
                     break;
             }
 
-            if ( field.isAnnotationPresent( Path.class ) ) {
-                path = field.getAnnotation( Path.class ).value();
+            if (field.isAnnotationPresent(Path.class)) {
+                path = field.getAnnotation(Path.class).value();
             }
 
-            if ( Modifier.isPrivate( field.getModifiers() ) ) {
-                field.setAccessible( true );
+            if (Modifier.isPrivate(field.getModifiers())) {
+                field.setAccessible(true);
             }
 
-            converter.fromConfig( (YamlConfig) this, field, ConfigSection.convertFromMap( section ), path );
+            converter.fromConfig((YamlConfig) this, field, ConfigSection.convertFromMap(section), path);
         }
     }
 
