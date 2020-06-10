e05066a5a158c588b18a2da2e72a8e7fc2ebea8c
Fabian Faßbender <fabian.fassbender42@googlemail.com>
2020-06-10 22:23:08 +0200

    Add facing block state sticky piston

--- gomint-api/src/main/java/io/gomint/world/block/BlockStickyPiston.java
+++ gomint-api/src/main/java/io/gomint/world/block/BlockStickyPiston.java
@@ -11,5 +11,5 @@ package io.gomint.world.block;
  * @author geNAZt
  * @version 1.0
  */
-public interface BlockStickyPiston extends Block {
+public interface BlockStickyPiston extends BlockFacing {
 }
--- gomint-server/src/main/java/io/gomint/server/world/block/StickyPiston.java
+++ gomint-server/src/main/java/io/gomint/server/world/block/StickyPiston.java
@@ -2,21 +2,20 @@ package io.gomint.server.world.block;
 
 import io.gomint.inventory.item.ItemStack;
 import io.gomint.server.world.block.helper.ToolPresets;
+import io.gomint.server.world.block.state.BlockfaceBlockState;
 import io.gomint.world.block.BlockType;
 
 import io.gomint.server.registry.RegisterInfo;
+import io.gomint.world.block.data.Facing;
 
 /**
  * @author geNAZt
  * @version 1.0
  */
-@RegisterInfo( sId = "minecraft:sticky_piston" )
+@RegisterInfo(sId = "minecraft:sticky_piston")
 public class StickyPiston extends Block implements io.gomint.world.block.BlockStickyPiston {
 
-    @Override
-    public String getBlockId() {
-        return "minecraft:sticky_piston";
-    }
+    private final BlockfaceBlockState facing = new BlockfaceBlockState(this, () -> new String[]{"facing_direction"});
 
     @Override
     public long getBreakTime() {
@@ -46,6 +45,16 @@ public class StickyPiston extends Block implements io.gomint.world.block.BlockSt
     @Override
     public Class<? extends ItemStack>[] getToolInterfaces() {
         return ToolPresets.PICKAXE;
+    }
+
+    @Override
+    public void setFacing(Facing facing) {
+        this.facing.setState(facing);
+    }
+
+    @Override
+    public Facing getFacing() {
+        return this.facing.getState();
     }
 
 }
