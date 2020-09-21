844277fbfbd27a8c129f747e30e3fc467e22ca1c
Pasqual Koschmieder <40468651+derklaro@users.noreply.github.com>
2020-09-21 16:42:13 +0200

    Add shared class between BlockSignNormal and BlockWallSign (#608)
    * Add shared class between WallSign and NormalSign
    
    * Rename BlockSignNormal to BlockStandingSign
    
    * Ensure that a sign lines are not more than 4 and prevent unmodifiable collections
    
    * Update gomint-api/src/main/java/io/gomint/event/world/SignChangeTextEvent.java
    
    Co-authored-by: Janmm14 <Janmm14@users.noreply.github.com>
    
    * Fix javadoc and ensure that the sign has always 4 lines of text
    
    * Fix the checking condition
    
    Co-authored-by: Fabian Faßbender <fabian.fassbender42@gmail.com>
    Co-authored-by: Janmm14 <Janmm14@users.noreply.github.com>

--- gomint-api/src/main/java/io/gomint/world/block/BlockWallSign.java
+++ gomint-api/src/main/java/io/gomint/world/block/BlockWallSign.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018 Gomint team
+ * Copyright (c) 2020 Gomint team
  *
  * This code is licensed under the BSD license found in the
  * LICENSE file in the root directory of this source tree.
@@ -7,54 +7,10 @@
 
 package io.gomint.world.block;
 
-import io.gomint.world.block.data.LogType;
-
-import java.util.List;
-
 /**
  * @author geNAZt
  * @version 1.0
  * @stability 1
  */
-public interface BlockWallSign extends BlockFacing {
-
-    /**
-     * Get a copy of all lines on this sign. The maximum size of
-     * this list is 4.
-     *
-     * @return list of all lines
-     */
-    List<String> getLines();
-
-    /**
-     * Set a specific line to new content. When you set line 2 and there is no other line
-     * line 1 will be empty string
-     *
-     * @param line which should be set
-     * @param content which should be set for the line given
-     */
-    void setLine( int line, String content );
-
-    /**
-     * Get a specific line of the sign content
-     *
-     * @param line which you want to get
-     * @return string or null when not set
-     */
-    String getLine( int line );
-
-    /**
-     * Get the type of wood from which this button has been made
-     *
-     * @return type of wood
-     */
-    LogType getWoodType();
-
-    /**
-     * Set the type of wood for this button
-     *
-     * @param logType type of wood
-     */
-    void setWoodType(LogType logType);
-
+public interface BlockWallSign extends BlockSign, BlockFacing {
 }
