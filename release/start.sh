d0c8891a396f86d887c054dd3e3261d1b5207d24
Fabian Faßbender <fabian.fassbender42@googlemail.com>
2020-09-22 12:02:29 +0200

    Add default profiler to startup

--- release/start.sh
+++ release/start.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-java --add-opens java.base/java.nio=io.netty.common --add-exports java.base/jdk.internal.misc=io.netty.common -p modules -m gomint.server/io.gomint.server.Bootstrap
+java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005 --add-opens java.base/java.nio=io.netty.common --add-exports java.base/jdk.internal.misc=io.netty.common -p modules -m gomint.server/io.gomint.server.Bootstrap
