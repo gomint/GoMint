fc6489beef5793a650ffd0f83b2e9eba828631a5
Fabian Faßbender <fabian.fassbender42@gmail.com>
2020-09-25 16:34:24 +0200

    Remove the profiler again, we should implement some custom params (#640)
    * Add default profiler to startup
    
    * Remove debug stuff again
    
    * Delete unnamed.patch
    
    Co-authored-by: Fabian Faßbender <fabian.fassbender42@googlemail.com>

--- release/start.sh
+++ release/start.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
-java -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.rmi.port=9090 -Dcom.sun.management.jmxremote=true \
- -Dcom.sun.management.jmxremote.port=9090 -Dcom.sun.management.jmxremote.ssl=false \
- -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.local.only=false \
- -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005 --add-opens java.base/java.nio=io.netty.common \
+java --add-opens java.base/java.nio=io.netty.common \
  --add-exports java.base/jdk.internal.misc=io.netty.common -p modules -m gomint.server/io.gomint.server.Bootstrap
