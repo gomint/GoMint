07e1d238ebccc10852c3fa0149e0ff5b4b5acc4e
Kaooot <jonapapenfuss@gmail.com>
2020-09-27 18:35:21 +0200

    provide requested changes

--- gomint-server/src/main/java/io/gomint/server/command/gomint/PluginsCommand.java
+++ gomint-server/src/main/java/io/gomint/server/command/gomint/PluginsCommand.java
@@ -27,14 +27,14 @@ public class PluginsCommand extends Command {
     public CommandOutput execute(CommandSender commandSender, String alias, Map<String, Object> arguments) {
         Map<String, Plugin> plugins = GoMint.instance().getPluginManager().getPlugins();
 
-        if (plugins.size() == 0) {
+        if (plugins.isEmpty()) {
             return CommandOutput.failure("No plugins were loaded.");
         }
 
-        StringBuilder stringBuilder = new StringBuilder("Plugins (" + "§a" + plugins.size() + "§r): ");
+        StringBuilder stringBuilder = new StringBuilder("Plugins (§a" + plugins.size() + "§r): §a");
 
         for (Plugin plugin : plugins.values()) {
-            stringBuilder.append("§a").append(plugin.getName()).append(" v").append(plugin.getVersion().toString()).append("§r, ");
+            stringBuilder.append(plugin.getName()).append("§r, ");
         }
 
         stringBuilder.setLength(stringBuilder.length() - 2);
