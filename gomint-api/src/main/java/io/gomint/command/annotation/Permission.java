83936e89271395e52745d778ef8ad4244cc597cf
Fabian Faßbender <fabian.fassbender42@gmail.com>
2020-11-24 15:15:16 +0100

    Update copyright from 2016...2018 to 2020 (merges #669)
    * Entity reworking for spawning
    
    * Cleanup dye item and fix relative entity movement (it has no use now anymore)
    
    * Move glazed terracotta into one API interface (like wool and dye)
    Re-enable "relative" movement
    
    * Update copyright
    
    Co-authored-by: Fabian Faßbender <fabian.fassbender42@googlemail.com>

--- gomint-api/src/main/java/io/gomint/command/annotation/Permission.java
+++ gomint-api/src/main/java/io/gomint/command/annotation/Permission.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018 GoMint team
+ * Copyright (c) 2020 GoMint team
  *
  * This code is licensed under the BSD license found in the
  * LICENSE file in the root directory of this source tree.
--- gomint-api/src/main/java/io/gomint/entity/potion/PotionEffect.java
+++ gomint-api/src/main/java/io/gomint/entity/potion/PotionEffect.java
@@ -1,6 +1,6 @@
 
 /*
- * Copyright (c) 2017, GoMint, BlackyPaw and geNAZt
+ * Copyright (c) 2020, GoMint, BlackyPaw and geNAZt
  *
  * This code is licensed under the BSD license found in the
  * LICENSE file in the root directory of this source tree.
