feba5081d2194a219b832bed6889061e38e4ce8a
Fabian Faßbender <fabian.fassbender42@gmail.com>
2021-01-09 16:35:51 +0100

    Remove watchdog from sentry (#686)
    Add checkBounds to false for better memory performance
    
    Co-authored-by: Fabian Faßbender <fabian.fassbender42@googlemail.com>

--- release/start.cmd
+++ release/start.cmd
@@ -1,2 +1,2 @@
 @echo off
-java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005 --add-opens java.base/java.nio=io.netty.common --add-exports java.base/jdk.internal.misc=io.netty.common -p modules -m gomint.server/io.gomint.server.Bootstrap
+java -Dio.netty.buffer.checkBounds=false --add-opens java.base/java.nio=io.netty.common --add-exports java.base/jdk.internal.misc=io.netty.common -p modules -m gomint.server/io.gomint.server.Bootstrap
