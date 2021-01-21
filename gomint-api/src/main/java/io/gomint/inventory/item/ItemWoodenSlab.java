7428e1140c74191cb258fc202d09afbe3fb1d802
jihuayu <i@jihuayu.site>
2021-01-21 12:59:07 +0800

    Fix ItemWoodenSlab type and top

--- gomint-api/src/main/java/io/gomint/inventory/item/ItemWoodenSlab.java
+++ gomint-api/src/main/java/io/gomint/inventory/item/ItemWoodenSlab.java
@@ -1,13 +1,14 @@
 package io.gomint.inventory.item;
 
 import io.gomint.GoMint;
+import io.gomint.world.block.data.LogType;
 
 /**
  * @author geNAZt
  * @version 1.0
  * @stability 2
  */
-public interface ItemWoodenSlab extends ItemStack<ItemWoodenSlab>, ItemBurnable {
+public interface ItemWoodenSlab extends ItemSlab<ItemWoodenSlab>, ItemBurnable {
 
     /**
      * Create a new item stack with given class and amount
@@ -19,4 +20,18 @@ public interface ItemWoodenSlab extends ItemStack<ItemWoodenSlab>, ItemBurnable 
         return GoMint.instance().createItemStack( ItemWoodenSlab.class, amount );
     }
 
+    /**
+     * Get the type of wooden this slab has
+     *
+     * @return type of wooden
+     */
+    LogType type();
+
+    /**
+     * Set the type of wooden for this block
+     *
+     * @param type for this block
+     * @return block for chaining
+     */
+    ItemWoodenSlab type(LogType type);
 }
