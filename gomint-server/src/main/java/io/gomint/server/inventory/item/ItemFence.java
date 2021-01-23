5df114332dca7486d45d41a24b75abf06c8b5e12
jihuayu <i@jihuayu.site>
2021-01-23 11:06:37 +0800

    reface FenceGate

--- gomint-server/src/main/java/io/gomint/server/inventory/item/ItemFence.java
+++ gomint-server/src/main/java/io/gomint/server/inventory/item/ItemFence.java
@@ -1,10 +1,8 @@
 package io.gomint.server.inventory.item;
 
 import io.gomint.inventory.item.ItemType;
-
 import io.gomint.server.registry.RegisterInfo;
 import io.gomint.server.world.block.Fence;
-import io.gomint.server.world.block.Leaves;
 import io.gomint.world.block.Block;
 import io.gomint.world.block.data.LogType;
 
@@ -15,6 +13,9 @@ import java.time.Duration;
  * @version 1.0
  */
 @RegisterInfo(sId = "minecraft:fence")
+@RegisterInfo(sId = "minecraft:crimson_fence")
+@RegisterInfo(sId = "minecraft:warped_fence")
+
 public class ItemFence extends ItemStack<io.gomint.inventory.item.ItemFence> implements io.gomint.inventory.item.ItemFence {
     private enum FenceType {
         OAK("minecraft:fence", (short) 0),
@@ -23,8 +24,8 @@ public class ItemFence extends ItemStack<io.gomint.inventory.item.ItemFence> imp
         JUNGLE("minecraft:fence", (short) 3),
         ACACIA("minecraft:fence", (short) 4),
         DARK_OAK("minecraft:fence", (short) 5),
-        CRIMSON("", (short) -1),
-        WARPED("", (short) -1);
+        CRIMSON("minecraft:crimson_fence", (short) -1),
+        WARPED("minecraft:warped_fence", (short) -1);
 
         public String id;
         public short data;
@@ -34,6 +35,7 @@ public class ItemFence extends ItemStack<io.gomint.inventory.item.ItemFence> imp
             this.id = id;
         }
     }
+
     @Override
     public ItemType itemType() {
         return ItemType.FENCE;
@@ -41,21 +43,27 @@ public class ItemFence extends ItemStack<io.gomint.inventory.item.ItemFence> imp
 
     @Override
     public Duration burnTime() {
-        return Duration.ofMillis(15000);
+        if (this.data() != -1)
+            return Duration.ofMillis(15000);
+        return null;
     }
 
     @Override
     public io.gomint.inventory.item.ItemFence type(LogType type) {
         FenceType type1 = FenceType.valueOf(type.name());
-        this.material(type.name());
-        this.data(type1.data);
-      return this;
+        this.material(type1.name());
+        if (type1.data != -1) {
+            this.data(type1.data);
+        } else {
+            this.data((short) 0);
+        }
+        return this;
     }
 
     @Override
     public LogType type() {
         for (FenceType value : FenceType.values()) {
-            if ((value.id.equals(this.material()) && this.data() == value.data)) {
+            if ((value.id.equals(this.material()) && (this.data() == value.data || value.data == -1))) {
                 return LogType.valueOf(value.name());
             }
         }
