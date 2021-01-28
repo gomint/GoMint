8414f067cd9cfa6df56cd60ef65f931318d0d4d1
Janmm14 <gitconfig1@janmm14.de>
2021-01-28 22:23:21 +0100

    Outsource event caller class creator code to static helper method

--- gomint-server/src/main/java/io/gomint/server/event/EventHandlerMethod.java
+++ gomint-server/src/main/java/io/gomint/server/event/EventHandlerMethod.java
@@ -12,9 +12,6 @@ import io.gomint.event.EventHandler;
 import io.gomint.event.EventListener;
 import io.gomint.server.maintenance.ReportUploader;
 import io.gomint.server.plugin.PluginClassloader;
-import org.objectweb.asm.ClassWriter;
-import org.objectweb.asm.MethodVisitor;
-import org.objectweb.asm.Opcodes;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -53,45 +50,7 @@ class EventHandlerMethod implements Comparable<EventHandlerMethod> {
         // Build up proxy
         try {
             if (instance.getClass().getClassLoader() instanceof PluginClassloader) {
-                ClassWriter cw = new ClassWriter(ClassWriter.COMPUTE_FRAMES);
-
-                String listenerClassName = instance.getClass().getName().replace(".", "/");
-                String className = listenerClassName + "Proxy" + PROXY_COUNT.incrementAndGet();
-                String eventClassName = method.getParameterTypes()[0].getName().replace(".", "/");
-
-                // Define the class
-                cw.visit(Opcodes.V11,
-                    Opcodes.ACC_PUBLIC,
-                    className,
-                    null,
-                    "java/lang/Object",
-                    new String[]{"io/gomint/server/event/EventProxy"});
-
-                // Define the obj field
-                cw.newField(className, "obj", "L" + listenerClassName + ";");
-                cw.visitField(Opcodes.ACC_PUBLIC, "obj", "L" + listenerClassName + ";", null, null);
-
-                // Build constructor
-                MethodVisitor con = cw.visitMethod(Opcodes.ACC_PUBLIC, "<init>", "()V", null, null);
-                con.visitCode();
-                con.visitVarInsn(Opcodes.ALOAD, 0);
-                con.visitMethodInsn(Opcodes.INVOKESPECIAL, "java/lang/Object", "<init>", "()V", false);
-                con.visitInsn(Opcodes.RETURN);
-                con.visitMaxs(1, 1);
-
-                // Build call method
-                MethodVisitor callCon = cw.visitMethod(Opcodes.ACC_PUBLIC, "call", "(Lio/gomint/event/Event;)V", null, null);
-                callCon.visitCode();
-                callCon.visitVarInsn(Opcodes.ALOAD, 0);
-                callCon.visitFieldInsn(Opcodes.GETFIELD, className, "obj", "L" + listenerClassName + ";");
-                callCon.visitVarInsn(Opcodes.ALOAD, 1);
-                callCon.visitTypeInsn(Opcodes.CHECKCAST, eventClassName);
-                callCon.visitMethodInsn(Opcodes.INVOKEVIRTUAL, listenerClassName, method.getName(), "(L" + eventClassName + ";)V", false);
-                callCon.visitInsn(Opcodes.RETURN);
-                callCon.visitMaxs(2, 2);
-
-                // Get bytecode
-                byte[] data = cw.toByteArray();
+                byte[] data = EventCallerClassCreator.createClass(instance, method, PROXY_COUNT.incrementAndGet());
 
                 MethodHandles.Lookup lookup = MethodHandles.lookup();
                 MethodHandles.Lookup privateLookup = MethodHandles.privateLookupIn(instance.getClass(), lookup);
