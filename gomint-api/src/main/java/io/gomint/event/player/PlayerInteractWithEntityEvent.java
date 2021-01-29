78dd5957ac24fa5384cbaade256d21ac58f4a132
Janmm14 <gitconfig1@janmm14.de>
2021-01-29 17:07:30 +0100

    Javadoc improvement for PlayerInteractWithEntityEvent

--- gomint-api/src/main/java/io/gomint/event/player/PlayerInteractWithEntityEvent.java
+++ gomint-api/src/main/java/io/gomint/event/player/PlayerInteractWithEntityEvent.java
@@ -38,6 +38,8 @@ public class PlayerInteractWithEntityEvent extends CancellablePlayerEvent<Player
      * <b>Be careful! This method used to return the entity interacted with before 2.0</b>
      * 
      * @return the player which is affected by this event
+     * @see #player() 
+     * @see #interactEntity() 
      */
     @Override
     @Deprecated(since = "2.0")
